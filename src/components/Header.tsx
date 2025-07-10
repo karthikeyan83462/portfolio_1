a281b2427d109a8f5c265e665d6102b7b88f7a10
Karthikeyan <karthikeyan83462@gmail.com>
2025-07-10 17:39:23 +0530

    testing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
       <Nav>
         <Link href="/" passHref>
           <Logo whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-            Karthikeyan
+            Karthikeyan A
           </Logo>
         </Link>
 
